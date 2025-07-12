ab3357c4b124e972840c4a2fdb59a8dfecb6b6b2
Trung Dinh <xamarindevelopervietnam@outlook.com>
2025-07-12 09:09:25 +0700

    change output directory

--- build-sandbox-runtime.sh
+++ build-sandbox-runtime.sh
@@ -1,3 +1,4 @@
+mkdir -p containers/runtime && \
 cd coreservices && poetry run python3 openhands/runtime/utils/runtime_build.py \
     --base_image nikolaik/python-nodejs:python3.13-nodejs24 \
-    --build_folder containers/runtime
+    --build_folder ../containers/runtime
