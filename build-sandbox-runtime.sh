06d20f4a961d0662c4dbdd0ef6ad013460682b18
Trung Dinh <xamarindevelopervietnam@outlook.com>
2025-07-12 08:50:44 +0700

    Use image `nikolaik/python-nodejs:python3.13-nodejs24`

--- build-sandbox-runtime.sh
+++ build-sandbox-runtime.sh
@@ -1,3 +1,3 @@
 cd coreservices && poetry run python3 openhands/runtime/utils/runtime_build.py \
-    --base_image nikolaik/python-nodejs:python3.12-nodejs22 \
+    --base_image nikolaik/python-nodejs:python3.13-nodejs24 \
     --build_folder containers/runtime
